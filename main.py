7af30aa3e52ec8bd8dca6f9743d76ac2fd2491cf
Tzvika <maizlin92@gmail.com>
2026-08-03 19:19:58 +0300

    Initial commit. add flask home and about web pages.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
 
-app = Flask("Website")
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+
+@app.route("/api/v1/<word>")
+def apicall(station, date):
+    filename = "data_big/TG_STAID" + str(station).zfill(6) + ".txt"
+    pd.read_csv("data_big/yossi.txt")
+
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df['TG0'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
+    df['TG'] = df['TG0'] / 10
+    temperature = df.loc[df['    DATE']==date]['TG'].squeeze()
+    result = {"Station": station, "Date": date, "Temperature": temperature}
+    return result
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
