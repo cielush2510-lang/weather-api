936161398957bb1a777bd427114ef190320009ef
Tzvika <maizlin92@gmail.com>
2026-08-13 14:53:48 +0300

    improve code

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 
 
+def get_station(station):
+    filename = "data_big/stations.txt"
+    df_local = pd.read_csv(filename, skiprows=17)
+    station_name = df_local.loc[df_local['STAID']==station]['STANAME                                 '].squeeze().strip()
+    return station_name
+
+
 app = Flask(__name__)
 
 
@@ -11,16 +18,16 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<word>")
+@app.route("/api/v1/<int:station>/<date>")
 def apicall(station, date):
     filename = "data_big/TG_STAID" + str(station).zfill(6) + ".txt"
-    pd.read_csv("data_big/yossi.txt")
-
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     df['TG0'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
     df['TG'] = df['TG0'] / 10
     temperature = df.loc[df['    DATE']==date]['TG'].squeeze()
-    result = {"Station": station, "Date": date, "Temperature": temperature}
+    fahrenheit = round(temperature * (9 / 5) + 32, 2)
+    station_name = get_station(station)
+    result = {"Station": station_name, "Date": date, "Temperature": temperature, "Fahrenheit": fahrenheit}
     return result
 
 
