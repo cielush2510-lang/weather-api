811db44afaab90c88711e3af5e10fbf561b7a4f7
Tzvika <maizlin92@gmail.com>
2026-08-18 21:25:45 +0300

    Add URL endpoints for yearly and stations specific data

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ def get_station(station):
 app = Flask(__name__)
 
 
+stations = pd.read_csv("data_big/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<int:station>/<date>")
@@ -29,6 +33,23 @@ def apicall(station, date):
     station_name = get_station(station)
     result = {"Station": station_name, "Date": date, "Temperature": temperature, "Fahrenheit": fahrenheit}
     return result
+
+
+@app.route("/api/v1/<int:station>")
+def all_data(station):
+    filename = "data_big/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<int:station>/<year>")
+def year_data(station, year):
+    filename = "data_big/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    results = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return results
 
 
 if __name__ == "__main__":
